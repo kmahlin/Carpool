091992d812a935b8dab908c50bd4498dc894b274
kmahlin <kmahlin@gmail.com>
2014-04-05 23:41:13 -0500

    Added Moq to test project

---
+++ CarpoolSystem.Tests/AccountControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using CarpoolSystem.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using System.Web.Mvc;
+using CarpoolSystem.Models;
+
+namespace CarpoolSystem.Tests
+{
+
+    [TestClass()]
+    public class AccountControllerTest
+    {
+
+
+
+
+
+    }
+}
---
+++ CarpoolSystem.Tests/HomeControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using CarpoolSystem.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using System.Web.Mvc;
+using CarpoolSystem.Models;
+
+namespace CarpoolSystem.Tests
+{
+
+    [TestClass()]
+    public class HomeControllerTest
+    {
+
+
+    }
+}
