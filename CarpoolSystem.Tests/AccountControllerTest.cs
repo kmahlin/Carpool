2bee3c5741bc943ab2d26a6c21311dfa223b036d
kmahlin <kmahlin@gmail.com>
2014-04-08 16:51:28 -0500

    mocked user

--- CarpoolSystem.Tests/AccountControllerTest.cs
+++ CarpoolSystem.Tests/AccountControllerTest.cs
@@ -4,6 +4,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
 using System.Web.Mvc;
 using CarpoolSystem.Models;
+using Moq;
 
 namespace CarpoolSystem.Tests
 {
@@ -11,7 +12,32 @@ namespace CarpoolSystem.Tests
     [TestClass()]
     public class AccountControllerTest
     {
+        AccountController CreateControllerAs(string userName)
+        {
 
+            var mock = new Mock<ControllerContext>();
+            mock.SetupGet(p => p.HttpContext.User.Identity.Name).Returns(userName);
+            mock.SetupGet(p => p.HttpContext.Request.IsAuthenticated).Returns(true);
+
+            var controller = new AccountController();
+            controller.ControllerContext = mock.Object;
+
+            return controller;
+        }
+
+        [TestMethod]
+        public void EditAction_Should_Return_EditView_When_ValidOwner()
+        {
+
+            // Arrange
+            var controller = CreateControllerAs("SomeUser");
+
+            // Act
+            var result = controller.Profile() as ViewResult;
+
+            // Assert
+            Assert.IsInstanceOfType(result.ViewData.Model, typeof(ProfileModel));
+        }
 
 
 
--- CarpoolSystem.Tests/HomeControllerTest.cs
+++ CarpoolSystem.Tests/HomeControllerTest.cs
@@ -6,14 +6,15 @@ using CarpoolSystem.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
 using System.Collections.Specialized;
-using  System.Globalization;
+using System.Globalization;
 
 namespace CarpoolSystem.Tests
 {
 
     [TestClass()]
     public class HomeControllerTest
-    {
+    { 
+
         [TestMethod]
         public void Test_Event_ModelState_Invalid()
         {
@@ -31,6 +32,70 @@ namespace CarpoolSystem.Tests
                                     new NameValueCollection(), CultureInfo.InvariantCulture)
             };
 
+
+            //Act
+            var binder = new DefaultModelBinder().BindModel(
+                 new ControllerContext(), modelBinder);
+            controller.ModelState.Clear();
+            controller.ModelState.Merge(modelBinder.ModelState);
+
+            ViewResult result = (ViewResult)controller.Event(model);
+
+            //Assert
+            Assert.IsTrue(!result.ViewData.ModelState.IsValid);
+
+        }
+
+        [TestMethod]
+        //Will break because database isn't mocked
+        public void Test_Event_ModelState_valid()
+        {
+
+
+
+            //Arrange 
+            var accountController = new AccountController();
+
+
+
+            var loginModel = new LogInModel()
+            {
+                UserName = "admin",
+                Password = "123456",
+            };
+            accountController.LogIn(loginModel);
+
+            var controller = new HomeController();
+            controller.ViewData.ModelState.Clear();
+
+            var model = new EventModel()
+            {
+                Title = "Event1",
+                StartingAddress = "Street1",
+                StartingCity = "Lincoln",
+                StartingState = "Nebraska",
+                DestAddress = "Street2",
+                DestCity = "Omaha",
+                DestState = "Nebraska",
+                StartingTime = " 10:00Am",
+                EndingTime = "5:00pm",
+                EventInfo = "stuff and stuff",
+                Days = "MWFT",
+                CarMake = "Ford",
+                CarModel = "F150",
+                CarYear = 2015,
+                CarColor = "Grey",
+                TotalSeats = 5,
+            };
+
+
+            var modelBinder = new ModelBindingContext()
+            {
+                ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(
+                                  () => model, model.GetType()),
+                ValueProvider = new NameValueCollectionValueProvider(
+                                    new NameValueCollection(), CultureInfo.InvariantCulture)
+            };
 
             //Act
             var binder = new DefaultModelBinder().BindModel(
