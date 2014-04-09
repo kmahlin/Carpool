be8b1b84d522f757fef1f562033766fafd84fbac
yyang15 <yuekun.yangy@gmail.com>
2014-04-09 16:32:12 -0500

    returnView update and LogIn invilid test

--- CarpoolSystem.Tests/AccountControllerTest.cs
+++ CarpoolSystem.Tests/AccountControllerTest.cs
@@ -4,6 +4,8 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
 using System.Web.Mvc;
 using CarpoolSystem.Models;
+using System.Collections.Specialized;
+using System.Globalization;
 
 namespace CarpoolSystem.Tests
 {
@@ -11,7 +13,36 @@ namespace CarpoolSystem.Tests
     [TestClass()]
     public class AccountControllerTest
     {
+        [TestMethod]
+        public void Test_Login_ModelState_Invalid()
+        {
+            //Arrange 
+            var controller = new AccountController();
+            controller.ViewData.ModelState.Clear();
 
+            var model = new LogInModel();
+
+            var modelBinder = new ModelBindingContext()
+            {
+                ModelMetadata = ModelMetadataProviders.Current.GetMetadataForType(
+                                  () => model, model.GetType()),
+                ValueProvider = new NameValueCollectionValueProvider(
+                                    new NameValueCollection(), CultureInfo.InvariantCulture)
+            };
+
+
+            //Act
+            var binder = new DefaultModelBinder().BindModel(
+                 new ControllerContext(), modelBinder);
+            controller.ModelState.Clear();
+            controller.ModelState.Merge(modelBinder.ModelState);
+
+            ViewResult result = (ViewResult)controller.LogIn(model);
+
+            //Assert
+            Assert.IsTrue(!result.ViewData.ModelState.IsValid);
+
+        }
 
 
 
